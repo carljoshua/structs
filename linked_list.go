06cc241de9f570f5ea4282f4e6555cf551f5fa11
CarlJoshua <biag032299@gmail.com>
2017-08-05 23:45:54 +0800

    Fixed LinkedList

--- linked_list.go
+++ linked_list.go
@@ -1,27 +1,40 @@
 package structs
 
+import "fmt"
+
 type ListNode struct {
     val interface{}
     next *ListNode
+    isEmpty bool
 }
 
 func NewList() *ListNode{
-    return &ListNode{}
+    return &ListNode{isEmpty: true}
 }
 
 func (l *ListNode) Insert(v interface{}, key int){
-    tmp := &ListNode{val: v}
-    if key == 0{
-        tmp.next = l
-        l = tmp
-        return
+    if l.isEmpty{
+        l.val = v
+        l.isEmpty = false
+    }else{
+        tmp := &ListNode{val: v}
+        if key == 0{
+            b_val := l.val
+            b_next := l.next
+
+            l.val = tmp.val
+            tmp.val = b_val
+            tmp.next = b_next
+            l.next = tmp
+        }else{
+            tmp2 := l
+            for i := 0; i < key - 1; i++{
+                tmp2 = tmp2.next
+            }
+            tmp.next = tmp2.next
+            tmp2.next = tmp
+        }
     }
-    tmp2 := l
-    for i := 0; i < key-1; i++{
-        tmp2 = tmp2.next
-    }
-    tmp.next = tmp2.next
-    tmp2.next = tmp
 }
 
 func (l *ListNode) Search(key int) interface{}{
@@ -30,4 +43,12 @@ func (l *ListNode) Search(key int) interface{}{
         temp = temp.next
     }
     return temp.val
+}
+
+func (l *ListNode) Print(){
+    temp := l
+    for temp != nil{
+        fmt.Printf("%s\n", temp)
+        temp = temp.next
+    }
 }
